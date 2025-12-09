bedf565dbdbe7738529534a71f6f2ad9f46322ce
Mecso2 <mecsbal@gmail.com>
2025-12-09 20:23:04 +0100

    untested Dijkstra's

--- code.zig
+++ code.zig
@@ -11,6 +11,9 @@ const Row = struct {
         self.items[self.count] = entry;
         self.count += 1;
     }
+    pub inline fn slice(self: *@This()) []Entry {
+        return self.items[0..self.count];
+    }
 };
 
 var data: [NUMBER_OF_EDGES]Row = .{Row{}} ** NUMBER_OF_EDGES;
@@ -41,3 +44,47 @@ pub fn main() !void {
     const a: f32 = @floatFromInt(std.time.nanoTimestamp() - start_time);
     std.debug.print("finished reading {}", .{a / 1_000_000});
 }
+
+pub fn dijkstra(allocator: std.mem.Allocator, from: u32, to: u32) !?[]u32 {
+    assert(from != to);
+    const T = struct {
+        cost: f32,
+        id: u32,
+        prev: u32,
+        fn compareFn(_: void, a: @This(), b: @This()) std.math.Order {
+            return std.math.order(a.cost, b.cost);
+        }
+    };
+    var pq: std.PriorityQueue(T, void, T.compareFn) = .init(allocator, {});
+    defer pq.deinit();
+    var visited_from: std.AutoHashMap(u32, u32) = .init(allocator);
+    defer visited_from.deinit();
+
+    try pq.add(.{ .cost = 0, .id = from, .prev = undefined });
+    while (pq.removeOrNull()) |e| {
+        if (e.id == to) {
+            var path: std.ArrayList(u32) = .empty;
+            errdefer path.deinit(allocator);
+
+            try path.append(allocator, to);
+            var id: u32 = e.prev;
+            while (id != from) {
+                try path.append(allocator, id);
+                id = visited_from.get(id).?;
+            }
+            try path.append(allocator, from);
+            std.mem.reverse(u32, path.items);
+
+            return path.toOwnedSlice(allocator);
+        }
+        const gop = try visited_from.getOrPut(e.id);
+        if (gop.found_existing) continue;
+        gop.value_ptr.* = e.prev;
+
+        for (data[e.id].slice()) |c| {
+            try pq.add(T{ .cost = e.cost + c.weight, .id = c.node, .prev = e.id });
+        }
+    }
+    return null;
+}
+
